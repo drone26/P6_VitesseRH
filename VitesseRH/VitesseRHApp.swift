2af9d108d1697e3d4db25fce295afbb76d494f02
Mathieu ARRIO <minimat26git@arrio.fr>
2026-02-07 20:45:32 +0100

    Initial Commit

---
+++ VitesseRH/VitesseRHApp.swift
@@ -0,0 +1,17 @@
+//
+//  VitesseRHApp.swift
+//  VitesseRH
+//
+//  Created by Mathieu ARRIO on 07/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct VitesseRHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
