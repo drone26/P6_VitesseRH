16da063fe3a270d9663e0f0d457128b488c48c6c
Mathieu ARRIO <minimat26git@arrio.fr>
2026-02-08 23:14:21 +0100

    fix(utils): email with length of domain < 2 is invalid

--- VitesseRH/Common/Validators/Format.swift
+++ VitesseRH/Common/Validators/Format.swift
@@ -11,7 +11,7 @@ import Foundation
 /// - Returns: result of RFC 5322 compliance test
 func isValidEmailFormat(_ email: String) -> Bool {
     // email regex format RFC 5322 compliant
-    let emailRegex = #/(?:[a-zA-Z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+\/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?/#
+    let emailRegex = #/(?:[a-zA-Z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+\/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])/#
     return (try? emailRegex.wholeMatch(in: email)) != nil
 }
 
